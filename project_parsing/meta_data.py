88c66f16dc3a1fee7ad22b1535725f7065868a7f
nurzhank <asapnrz@gmail.com>
2024-02-28 23:20:59 +0600

    commit update

---
+++ project_parsing/meta_data.py
@@ -0,0 +1,10 @@
+URLS = {
+    'Sulpak': 'https://www.sulpak.kz/f/noutbuki/almaty/846_62?selectedTagsTokens=1198',
+    'Technodom': 'https://www.technodom.kz/catalog/noutbuki-i-komp-jutery/noutbuki-i-aksessuary/noutbuki/f/brands/apple',
+    'Alser': 'https://alser.kz/c/applemacbook'
+}
+HEADER = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0'
+}
+
